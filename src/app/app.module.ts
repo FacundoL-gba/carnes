4299e617ac1629599cc0f568faa5672f881ee00a
MsMapple <81583351+MsMapple@users.noreply.github.com>
2022-05-03 11:05:53 -0300

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './meterial/material.module'
+import { MaterialModule } from './meterial/material.module';
+import { NavbarComponent } from './shared/navbar/navbar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
