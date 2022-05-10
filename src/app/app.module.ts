28ff2120f48758a26bacc826a03c874d3e7a8fee
MsMapple <81583351+MsMapple@users.noreply.github.com>
2022-05-10 10:58:54 -0300

    navbar footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './meterial/material.module';
-import { NavbarComponent } from './shared/navbar/navbar.component'
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component'
+import { ComponenteModule } from './componente/componente.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    ComponenteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
