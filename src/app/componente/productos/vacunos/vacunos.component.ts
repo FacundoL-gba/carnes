8587d58efb58150a25ae279ae81f0495e0bd3c42
MsMapple <81583351+MsMapple@users.noreply.github.com>
2022-05-30 09:36:28 -0300

    cambios hechos con ampi

--- src/app/componente/productos/vacunos/vacunos.component.ts
+++ src/app/componente/productos/vacunos/vacunos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ofertas } from 'src/app/models/ofertas';
+
 
 @Component({
   selector: 'app-vacunos',
@@ -7,7 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VacunosComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Ofertas [] = []
+  constructor(){ 
+    this.ofertas = [{
+        nombre:'Pata Y Muslo',
+        precioa:'$390',
+        oferta: '$300 Por KG',
+        cantidad: 'Mínimo de compra: 1KG',
+        imagen: 'https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/p/a/pata-muslo.jpg'
+      },
+      {
+        nombre:'Tapa De Asado',
+        precioa:'$1.100',
+        oferta: '$900 Por KG',
+        cantidad: 'Mínimo de compra: 1KG',
+        imagen: 'https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/t/a/tapa-de-asado.jpg'
+      },
+      {
+        nombre:'Cuadril de Cerdo',
+        precioa:'$795',
+        oferta: '$700 Por KG',
+        cantidad: 'Mínimo de compra: 1KG',
+        imagen: 'https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/c/u/cuadril-cerdo_1.jpg'
+      },
+      {
+        nombre:'Milanesa De Carne',
+        precioa:'$950',
+        oferta: '$800 Por KG',
+        cantidad: 'Mínimo de compra: 1KG',
+        imagen: 'https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/m/i/milanesas-carne.jpg'
+      },
+      {
+        nombre:'Picada Grande',
+        precioa:'$695',
+        oferta: '$600 Combo Por Unidad',
+        cantidad: 'Mínimo de compra: 1U',
+        imagen: 'https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/p/i/picada-grande.jpg'
+      },
+      {
+        nombre:'Asado Con Hueso',
+        precioa:'$1.200',
+        oferta: '$1.000 Por KG',
+        cantidad: 'Mínimo de compra: 1KG',
+        imagen: 'https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/a/s/asado-new.jpg'
+      }
+      ]
+    }
 
   ngOnInit(): void {
   }
